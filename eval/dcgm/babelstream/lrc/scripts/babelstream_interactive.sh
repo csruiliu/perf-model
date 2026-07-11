2a5be6ab25429b5d697a778c1d2bb1da01c11127
csruiliu <rui.liu@proton.me>
2026-07-11 06:41:27 -0700

    add scripts for babelstream on lrc

---
+++ eval/dcgm/babelstream/lrc/scripts/babelstream_interactive.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Allocating resources first, the following command is an example
+# salloc -p es2 -A pc_perfume -q es2_normal --nodes=1 --ntasks=1 --cpus-per-task=16 --gres=gpu:H100:1 -t 12:00:00
+
+# Start Singularity server first
+# singularity instance start --fakeroot --nv --writable-tmpfs --bind /tmp:/tmp --bind /global/home/users/rliu5:/global/home/users/rliu5 --network=none docker://nvidia/dcgm:4.4.1-2-ubuntu22.04 dcgm-instance
+
+# singularity exec instance://dcgm-instance nv-hostengine -n &
+
+#OpenMP settings:
+export OMP_NUM_THREADS=1
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
+
+# create results directory if not exist
+if [ ! -d "../results" ]; then
+  mkdir ../results
+fi
+
+export RESULTS_DIR=../results/BABELSTREAM_${SLURM_JOBID}
+
+export DCGM_SAMPLE_RATE=1000
+
+#Array size must be a multiple of 1024
+export ARRAYSIZE=100663296
+export NUMTIMES=100000 
+export BABELSTREAM="/pscratch/sd/r/ruiliu/BabelStream-5.0/build/cuda-stream"
+
+start=$(date +%s.%N)
+srun ./wrap_dcgmi_container.sh $BABELSTREAM -s $ARRAYSIZE -n $NUMTIMES > ${RESULTS_DIR}/babelstream-${SLURM_JOBID}.dcgmi
+end=$(date +%s.%N)
+elapsed=$(printf "%s - %s\n" $end $start | bc -l)
+
+printf "Elapsed Time: %.2f seconds\n" $elapsed > ${RESULTS_DIR}/runtime.out
+
