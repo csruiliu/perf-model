e2ef05509802ccf744b041c257ee49a4b08ff6fb
csruiliu <rui.liu@proton.me>
2026-08-05 16:28:07 -0500

    add files for lammps on rtx8000

---
+++ eval/dcgm/lammps/lrc/build-lammps-lrc-rtx8000-fp32.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+module load gcc/11.4.0
+
+BASE_DIRECTORY=/global/scratch/users/rliu5/lammps-lrc-rtx8000-fp32
+SOURCE_DIRECTORY=${BASE_DIRECTORY}/lammps
+BUILD_DIRECTORY=${BASE_DIRECTORY}/build_lammps
+INSTALL_DIRECTORY=${BASE_DIRECTORY}/install_lammps
+
+cd $BASE_DIRECTORY
+if [ ! -d ${SOURCE_DIRECTORY} ]
+then
+  git clone https://github.com/lammps/lammps
+  git checkout 372c8cdabab05268417ae7627b0a3005a9da2642
+fi
+
+mkdir -p ${BUILD_DIRECTORY}
+mkdir -p ${INSTALL_DIRECTORY}
+
+cd ${BUILD_DIRECTORY}
+
+cmake ${SOURCE_DIRECTORY}/cmake \
+    -D BUILD_SHARED_LIBS=OFF \
+    -D CMAKE_INSTALL_PREFIX=${INSTALL_DIRECTORY} \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_VERBOSE_MAKEFILE=ON \
+    -D CMAKE_CXX_COMPILER=${SOURCE_DIRECTORY}/lib/kokkos/bin/nvcc_wrapper \
+    -D MPI_C_COMPILER=mpicc \
+    -D MPI_CXX_COMPILER=mpicxx \
+    -D BUILD_MPI=yes \
+    -D FFT=KISS \
+    -D FFT_KOKKOS=CUFFT \
+    -D PKG_ML-SNAP=yes \
+    -D PKG_KOKKOS=yes \
+    -D KOKKOS_PREC=single \
+    -D Kokkos_ENABLE_CUDA=yes \
+    -D Kokkos_ENABLE_SERIAL=yes \
+    -D Kokkos_ENABLE_IMPL_CUDA_MALLOC_ASYNC=OFF \
+    -D Kokkos_ARCH_TURING75=ON && \
+cmake --build ${BUILD_DIRECTORY} --target all -- -j4 && \
+cmake --build ${BUILD_DIRECTORY} --target install -- -j4
---
+++ eval/dcgm/lammps/lrc/small_1node_1gpu/rtx8000_fp32_interactive.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# the input specification
+spec=small
+nn=256
+BENCH_SPEC="\
+        -in common/in.snap.test \
+        -var snapdir common/2J8_W.SNAP \
+        -var nx $nn -var ny $nn -var nz $nn \
+        -var nsteps 100"
+
+RESULTS_DIR="/global/home/users/rliu5/perf-model/eval/dcgm/lammps/lrc/results/LPS_SMALL_FP32_${SLURM_JOB_ID}"
+
+LAMMPS_DIR="/global/scratch/users/rliu5/lammps-lrc-rtx8000-fp32"
+
+LAMMPS_COMM="/global/home/users/rliu5/perf-model/eval/dcgm/lammps/common"
+
+LAMMPS_LRC="/global/home/users/rliu5/perf-model/eval/dcgm/lammps/lrc"
+
+mkdir -p ${RESULTS_DIR}
+cd ${RESULTS_DIR}
+ln -s ${LAMMPS_COMM} .
+#ln -s ../../wrap_dcgmi.sh .
+
+# This is needed if LAMMPS is built using cmake.
+#install_dir="../../../install_PM"
+#export LD_LIBRARY_PATH=${install_dir}/lib64:$LD_LIBRARY_PATH
+EXE="${LAMMPS_DIR}/install_lammps/bin/lmp"
+
+# Match the build env.
+export MPICH_GPU_SUPPORT_ENABLED=1
+
+input="-k on g 1 -sf kk -pk kokkos newton on neigh half ${BENCH_SPEC} "
+
+start_time=$(date +%s.%N)
+srun -N 1 -n 1 -c 16 --gpus-per-node=1 --cpu-bind=cores $EXE $input > ${RESULTS_DIR}/${SLURM_JOB_ID}.out
+end_time=$(date +%s.%N)
+elapsed=$(printf "%s - %s\n" $end_time $start_time | bc -l)
+
+printf "Elapsed Time: %.2f seconds\n" $elapsed > ${RESULTS_DIR}/runtime.out
+
+unlink common
